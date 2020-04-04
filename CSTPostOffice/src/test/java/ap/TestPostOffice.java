73322e2832279455e207ca3633bb6bba378c5ab7
jhmfreitas <joao.h.marques.de.freitas@tecnico.ulisboa.pt>
2020-04-04 01:19:56 +0100

    Fixes test cases

--- CSTPostOffice/src/test/java/ap/TestPostOffice.java
+++ CSTPostOffice/src/test/java/ap/TestPostOffice.java
@@ -8,20 +8,34 @@ import java.util.*;
 public class TestPostOffice {
 
     @Test
-    public void test1(){
+    public void testValidPostOffice(){
+        Product p1 = new Product("prod1","description1",1,1);
+        p1.store(1);
+        Product p2 = new Product("prod2","description2",1,1);
+        p2.store(1);
+
         List<Product> products = new ArrayList<Product>();
-        products.add(new Product("prod1","description1",1,1));
-        products.add(new Product("prod2","description2",1,1));
+        products.add(p1);
+        products.add(p2);
+
         PostOffice po = new PostOffice(4, products);
         po.setMaxNumberOfProducts(4);
     }
 
     @Test
     public void testCtorWhenProductNameIsNotUnique(){
+        Product p1 = new Product("prod","description1",2,1);
+        p1.store(2);
+        Product p2 = new Product("prod","description2",2,1);
+        p2.store(2);
+        Product p3 = new Product("prod","description3",2,1);
+        p3.store(2);
+
         List<Product> products = new ArrayList<Product>();
-        products.add(new Product("prod","description1",2,1));
-        products.add(new Product("prod","description2",2,1));
-        products.add(new Product("prod","description3",2,1));
+        products.add(p1);
+        products.add(p2);
+        products.add(p3);
+
         PostOffice po = new PostOffice(4, products);
         po.setMaxNumberOfProducts(5);
     }
